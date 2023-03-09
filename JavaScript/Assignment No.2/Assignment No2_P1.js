3115f47abeb62586fbab46f25dbb561048ade192
Saniya <saniyam2k@gmail.com>
2023-03-09 08:15:29 -0800

    Assignment No.2

---
+++ JavaScript/Assignment No.2/Assignment No2_P1.js
@@ -0,0 +1,9 @@
+var no = 11;
+
+for (let i = 1; i <= 10; i++) {
+    const res = i * no;
+
+    console.log(`${res}`);
+}
+
+
---
+++ JavaScript/Assignment No.2/Assignment No2_P5.js
@@ -0,0 +1,4 @@
+var res = 2;
+for (let i = 1; i <= 10; i++) {
+   console.log(`${2 * i} ${2 * (11 - i)}`);
+}
